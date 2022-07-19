69e7a5e4b43c94b65f3257308dccf0cb861ad4a6
MaxKorolko <m.v.korolko@gmail.com>
2022-07-19 15:26:48 +0300

    збірка

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,36 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit';
-import {
-  persistStore,
-  persistReducer,
-  FLUSH,
-  REHYDRATE,
-  PAUSE,
-  PERSIST,
-  PURGE,
-  REGISTER,
-} from 'redux-persist';
+
 import contactsReducer from './contacts/contacts-reducer';
-import storage from 'redux-persist/lib/storage';
-
-const persistConfig = {
-  key: 'contacts',
-  storage: storage,
-  blacklist: ['filter'],
-};
-
-const persisterReducer = persistReducer(persistConfig, contactsReducer);
 
 export const store = configureStore({
   reducer: {
-    contacts: persisterReducer,
+    contacts: contactsReducer,
   },
-  middleware: getDefaultMiddleware =>
-    getDefaultMiddleware({
-      serializableCheck: {
-        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
-      },
-    }),
+
   devTools: process.env.NODE_ENV === 'development',
 });
-
-export const persistor = persistStore(store);
