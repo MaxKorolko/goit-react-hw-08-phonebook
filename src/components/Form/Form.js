f76164673bdc00df4b618c56bb39a5ffdba5983a
MaxKorolko <m.v.korolko@gmail.com>
2022-07-25 04:01:04 +0300

    btn

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -70,7 +70,7 @@ export default function Form() {
             required
           />
         </label>
-        <button type="submit">Add contact</button>
+        <button className={s.btn} type="submit">Add contact</button>
       </form>
       {isLoading && <Loader />}
     </>
