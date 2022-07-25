905c8449241788711da15b42aee9563d21f9cef5
MaxKorolko <m.v.korolko@gmail.com>
2022-07-25 10:46:59 +0300

    fix

--- src/pages/LoginPage.js
+++ src/pages/LoginPage.js
@@ -1,21 +1,18 @@
 import AuthForm from 'components/AuthForm/AuthForm';
 import { useLoginUserMutation } from 'redux/contacts/contactsAPI';
 import { useDispatch } from 'react-redux';
-import { useNavigate } from 'react-router-dom';
+
 import { setUser } from 'redux/contacts/contacts-actions';
 
 export default function LoginPage() {
   const [loginUser] = useLoginUserMutation();
   const dispatch = useDispatch();
-  const navigate = useNavigate();
 
   const handleSubmit = data => {
-    console.log(data);
     loginUser(data)
       .unwrap()
       .then(payload => {
         dispatch(setUser(payload));
-        // navigate('/', { replace: true });
       });
   };
 
